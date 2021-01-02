9acc629eaaf97516434c2a0a2f806ca63cf0e5ff
ali7even6ix <mabashogh76@gmail.com>
2021-01-02 09:46:38 +0330

    adding files

---
+++ src/components/rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Rank = ({name, entries}) => {
+    return (
+        <div className='center'>
+            <div className='f3 white'>
+                {`${name}, your current rank is ...`}
+            </div>
+            <div className='f1 white'>
+                {entries}
+            </div>
+        </div>
+
+    );
+}
+
+export default Rank;
